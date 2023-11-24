d220640fa99ff38030c00a5c3654282e9b9766aa
IsaiGonzalez08 <isaigonzalez482@gmail.com>
2023-11-24 00:43:06 -0600

    FoldersSaludPro

---
+++ src/Router/AppRouter.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+export const AppRouter = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='' element/>
+          <Route path='' element/>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+
