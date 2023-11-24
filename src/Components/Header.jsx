3c2f267185ef0c945169d320df81b87b8d2b62b2
IsaiGonzalez08 <isaigonzalez482@gmail.com>
2023-11-24 00:55:23 -0600

    HeaderAndLandingPage

---
+++ src/Components/Header.jsx
@@ -0,0 +1,9 @@
+export const Header = () => {
+    return(
+        <header className="w-full flex justify-around">
+            <div></div>
+            <div></div>
+            <div></div>
+        </header>
+    )
+}
