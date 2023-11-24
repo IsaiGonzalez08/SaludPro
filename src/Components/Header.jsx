43feb9a62746796d5201894d7b2f39de935f8b64
IsaiGonzalez08 <isaigonzalez482@gmail.com>
2023-11-24 01:08:08 -0600

    AddFontPoppins

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,21 @@
 export const Header = () => {
     return(
-        <header className="w-full flex justify-around">
-            <div></div>
-            <div></div>
-            <div></div>
+        <header className="w-full flex justify-around items-center">
+            <div className="flex">
+                <img src="" alt="" />
+                <h2 className="flex">
+                    <span className="text-[#4B4B4B]">Salud</span>
+                    <span className="text-[#4B4B4B]">Pro</span>
+                </h2>
+            </div>
+            <div>
+                <a className="text-[#6A6A6A] hover:font-semibold" href="">Inicio </a>
+                <a className="text-[#6A6A6A] hover:font-semibold" href="">Servicio</a>
+                <a className="text-[#6A6A6A] hover:font-semibold" href="">Nosotros</a>
+            </div>
+            <div>
+
+            </div>
         </header>
     )
 }
