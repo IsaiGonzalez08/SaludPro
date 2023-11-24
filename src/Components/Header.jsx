f91c03dde35c0ca5eb44aa48c09aa43a142fe038
IsaiGonzalez08 <isaigonzalez482@gmail.com>
2023-11-24 01:29:20 -0600

    AddHeaderComponent

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,20 +1,26 @@
+import Logo from '../assets/icon/Logo.svg'
+
 export const Header = () => {
     return(
-        <header className="w-full flex justify-around items-center">
-            <div className="flex">
-                <img src="" alt="" />
+        <header className="w-full h-[100px] flex justify-around items-center gap-32">
+            <div className="flex items-center gap-3">
+                <img src={Logo} alt="Logo header" />
                 <h2 className="flex">
-                    <span className="text-[#4B4B4B]">Salud</span>
-                    <span className="text-[#4B4B4B]">Pro</span>
+                    <span className="text-[#0D193E] font-light">Salud</span>
+                    <span className="text-[#0D193E] font-bold">Pro</span>
                 </h2>
             </div>
-            <div>
-                <a className="text-[#6A6A6A] hover:font-semibold" href="">Inicio </a>
-                <a className="text-[#6A6A6A] hover:font-semibold" href="">Servicio</a>
-                <a className="text-[#6A6A6A] hover:font-semibold" href="">Nosotros</a>
+            <div className='flex gap-10'>
+                <a className="text-[#6A6A6A] hover:font-bold hover:duration-300" href="">Inicio</a>
+                <a className="text-[#6A6A6A] hover:font-bold hover:duration-300" href="">Servicio</a>
+                <a className="text-[#6A6A6A] hover:font-bold hover:duration-300" href="">Nosotros</a>
             </div>
             <div>
-
+                <button className='text-[#FFFFFF] bg-[#0D193E] 
+                w-[120px] h-[35px] text-sm font-medium
+                hover:scale-105 hover:duration-300'>
+                Comenzar
+                </button>
             </div>
         </header>
     )
--- src/Router/AppRouter.jsx
+++ src/Router/AppRouter.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { LandingPage } from '../Pages/LandingPage'
 
 export const AppRouter = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='' element/>
+          <Route path='/' element={<LandingPage/>}/>
           <Route path='' element/>
         </Routes>
       </BrowserRouter>
